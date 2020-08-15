a8c2f4997c69c796f5d80613a48e547b6835fa20
Jason B <buquiranjason@gmail.com>
2020-08-15 19:42:02 +1200

    Updates

---
+++ precache-manifest.7ea9d9797337133ae9759063fd3530c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb0938085cb147d72a957680962afff",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "8e34bf3ae2410fcb2ec5",
+    "url": "/react-calculator/static/js/2.c58a99b3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-calculator/static/js/2.c58a99b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85905f6d7fe8a3efb014",
+    "url": "/react-calculator/static/js/main.a951f36b.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/precache-manifest.8fcadf73bb6157112b0dc5dfb40703a2.js"
+  "/react-calculator/precache-manifest.7ea9d9797337133ae9759063fd3530c6.js"
 );
 
 self.addEventListener('message', (event) => {
